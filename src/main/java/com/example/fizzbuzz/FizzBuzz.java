65ea2ef2b78eccb5756eb84dae524b6a3096f02a
Vladimir <vovas11@gmail.com>
2018-03-26 11:01:04 +0200

    Add working solution

--- src/main/java/com/example/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/example/fizzbuzz/FizzBuzz.java
@@ -7,21 +7,37 @@ public class FizzBuzz {
         new FizzBuzz().printFizzBuzz();
     }
 
+    /**
+     * Auxiliary method printing fizz/buzz/numbers from 1 to 100
+     */
     private void printFizzBuzz() {
         Arrays.stream(fizzBuzz(1, 100)).forEach(System.out::println);
     }
 
 
     /**
-     * Creates an array of strings containing fizz buzz numbers
+     * Creates an array of strings containing fizz/buzz/numbers
      *
-     * @param start first number
-     * @param end last number
+     * @param start first number, positive integer
+     * @param end last number, positive integer, greater than or equal to start
      *
-     * @return array of FizzBuzz numbers
+     * @return array of fizz/buzz/numbers
      */
     public String[] fizzBuzz(int start, int end) {
-        // TODO
-        return null;
+        String[] fbNums = new String[end - start + 1];
+
+        for (int i = 0; i < fbNums.length; i++) {
+            int currentN = start + i;
+            String stringToAdd = "";
+
+            if (currentN % 3 == 0) {
+                stringToAdd += "Fizz";
+            }
+            if (currentN % 5 == 0) {
+                stringToAdd += "Buzz";
+            }
+            fbNums[i] = stringToAdd.isEmpty() ? String.valueOf(currentN) : stringToAdd;
+        }
+        return fbNums;
     }
 }
--- src/test/java/com/example/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/example/fizzbuzz/FizzBuzzTest.java
@@ -3,6 +3,8 @@ package com.example.fizzbuzz;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertArrayEquals;
 
 public class FizzBuzzTest {
@@ -21,6 +23,16 @@ public class FizzBuzzTest {
 
     @Test
     public void testFizzBuzzPrintsFrom1to20() {
-        assertArrayEquals("Numbers from 1 to 20", fizzBuzz.fizzBuzz(1, 100), FB_NUM_1_20);
+        assertArrayEquals("Numbers from 1 to 20", fizzBuzz.fizzBuzz(1, 20), FB_NUM_1_20);
+    }
+
+    @Test
+    public void testFizzBuzzPrintsFrom5to16() {
+        assertArrayEquals("Numbers from 5 to 20", fizzBuzz.fizzBuzz(5, 16), Arrays.copyOfRange(FB_NUM_1_20, 4, 16));
+    }
+
+    @Test
+    public void testFizzBuzzPrintsFrom5to5() {
+        assertArrayEquals("Numbers from 5 to 5", fizzBuzz.fizzBuzz(5, 5), Arrays.copyOfRange(FB_NUM_1_20, 4, 5));
     }
 }
